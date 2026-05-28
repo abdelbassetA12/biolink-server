a1b7be630ca006ff029f517c16b71fef0adf6a40
abdelbassetA12 <abdelbassetelhajiri2020@gmail.com>
2026-05-28 16:41:02 +0100

    update project

--- utils/mail.js
+++ utils/mail.js
@@ -1,21 +1,16 @@
 const nodemailer = require("nodemailer");
-const dns = require("dns");
-
-dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+
+  host: "74.125.69.108",
   port: 465,
   secure: true,
 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  },
+  }
 
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000
 });
 
 transporter.verify((error, success) => {
