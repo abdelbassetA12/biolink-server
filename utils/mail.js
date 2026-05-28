bcc09a6a234c6744c5dc529abe02e4c035035f24
abdelbassetA12 <abdelbassetelhajiri2020@gmail.com>
2026-05-28 16:30:57 +0100

    update project

--- utils/mail.js
+++ utils/mail.js
@@ -1,4 +1,39 @@
+const nodemailer = require('nodemailer');
 
+const transporter = nodemailer.createTransport({
+
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  },
+
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000
+
+});
+
+transporter.verify((error, success) => {
+
+  if (error) {
+
+    console.log("MAIL ERROR:");
+    console.log(error);
+
+  } else {
+
+    console.log("MAIL SERVER READY");
+
+  }
+
+});
+
+module.exports = transporter;
+/*
 const nodemailer = require('nodemailer');
 
 
@@ -33,6 +68,7 @@ transporter.verify((error, success) => {
 
 
 module.exports = transporter;
+*/
 /*
 const nodemailer = require('nodemailer');
 
